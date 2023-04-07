d7b8940269b1dd7756e97c33d2279b168aee61b0
Arednel <89831257061@mail.ru>
2023-04-07 17:48:21 +0600

    1.0.0.2 Migrations fix 2

--- resources/views/Info.blade.php
+++ resources/views/Info.blade.php
@@ -37,8 +37,8 @@
     <br>
     <a href="/">← На главную</a>
 
-    <p>version 1.0.0.1 / 2023.04.7</p>
-    <p>Migrations fix</p>
+    <p>version 1.0.0.2 / 2023.04.7</p>
+    <p>Migrations fix 2</p>
     <p>Made by Evtifeev Artyom Vladislavovich / Arednel</p>
     <p>2023 &#169 Test Center</p>
 </body>
